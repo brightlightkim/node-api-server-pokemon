b0906b222a527e695ff301e7a0d75345650b4715
brightlightkim <taeyangk95@naver.com>
2022-03-19 12:42:36 -0600

    use all the details

--- poki/public/javascripts/app.js
+++ poki/public/javascripts/app.js
@@ -4,17 +4,19 @@ var app = new Vue({
   el: '#app',
   data: {
     pokis: [],
+    politics:[],
     pokiName: '',
     pokiURL: '',
   },
   created: function() {
     this.getpokis();
+    this.getpolitics();
   },
   methods: {
     async getpokis() {
       // `this` points to the vm instance
       console.log("get pokis");
-      var url = "http://yourserver:4200/pokemon";
+      var url = "http://localhost:8080/pokemon";
       try {
         let response = await axios.get(url);
         this.pokis = response.data;
@@ -24,6 +26,34 @@ var app = new Vue({
       catch (error) {
         console.log(error);
       }
+    },
+    async getpolitics() {
+      // `this` points to the vm instance
+      console.log("politics");
+      var url = "http://localhost:8080/politics";
+      try {
+        let response = await axios.get(url);
+        this.politics = response.data;
+        console.log(this.politics);
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+    addItem() {
+      var url = "http://localhost:8080/pokemon";
+      axios.post(url, {
+          name: this.pokiName,
+          avatarUrl: this.pokiURL
+        })
+        .then(response => {})
+        .catch(e => {
+          console.log(e);
+        });
+      this.pokiName = '';
+      this.pokiURL = '';
+      this.getpokis();
     },
   }
 });
